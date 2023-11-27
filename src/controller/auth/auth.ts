5601d8fded118e4d53dec792d56c96a4bb9cf58e
unknown <nasik7@ymail.com>
2023-11-27 21:39:30 +0530

    test auth

--- src/controller/auth/auth.ts
+++ src/controller/auth/auth.ts
@@ -112,10 +112,17 @@ export class AuthController {
       console.log("isProd : ", isProd);
 
       //isProd
+      // res.cookie(AUTH_TOKEN, token, {
+      //   httpOnly: false, // The cookie is not accessible via JavaScript
+      //   secure: isProd ? true : false, // Cookie is sent over HTTPS only
+      //   sameSite: isProd ? "none" : "lax", // Cookie is not sent with cross-site requests
+      //   maxAge: tokenExpiryTime, // Set the cookie's expiration time
+      // });
+
       res.cookie(AUTH_TOKEN, token, {
-        httpOnly: false, // The cookie is not accessible via JavaScript
-        secure: isProd ? true : false, // Cookie is sent over HTTPS only
-        sameSite: isProd ? "none" : "lax", // Cookie is not sent with cross-site requests
+        httpOnly: true, // The cookie is not accessible via JavaScript
+        secure: true, // Cookie is sent over HTTPS only
+        sameSite: "none", // Cookie is not sent with cross-site requests
         maxAge: tokenExpiryTime, // Set the cookie's expiration time
       });
 
