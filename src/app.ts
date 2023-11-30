cb573b422fe0b5d9f1b189c18dfc8bf5b0e2fbc5
unknown <nasik7@ymail.com>
2023-11-30 14:10:33 +0530

    cors added

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,10 @@ import cors from "cors";
 import morgan from "morgan";
 import { auth, wallet } from "./routes";
 import { notFound, serverError } from "./middleware";
-import { morganOption, IN_PROD, CLIENT_URL } from "./config";
+import { morganOption, IN_PROD, corsOptions } from "./config";
 import passport from "passport";
 import session from "express-session";
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 
 import "./config/passport";
 
@@ -18,13 +18,18 @@ export const createApp = () => {
   app.use(morgan("combined", morganOption));
 
   //config
-  app.use(cors({
-    origin: CLIENT_URL, // Client's URL
-    credentials: true
-  }));  //app.use(express.json());
+  app.use(
+    cors(
+      corsOptions
+      //   {
+      //   origin: CLIENT_URL, // Client's URL
+      //   credentials: true
+      // }
+    )
+  ); //app.use(express.json());
   app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
   app.use(bodyParser.json({ limit: "50mb" }));
-  app.use(cookieParser()); 
+  app.use(cookieParser());
   // app.use(express.static(path.join(__dirname, "public")));
 
   app.use(
